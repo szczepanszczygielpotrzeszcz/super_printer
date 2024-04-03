08e4ecb2a0e89360db4fdb102fad7a06cac9f602
Stefanos <skibimilian.maksynski@gmail.com>
2024-04-03 20:54:46 +0200

    Implement first version of printer

---
+++ print.py
@@ -0,0 +1,13 @@
+class Printer:
+	def __init__(self):
+		print("Init the printer.")
+		print()
+
+		self.data = 0
+	
+	def print(self, data):
+		print(data)
+	
+	def print_nl(self, data):
+		print(data)
+		print()
