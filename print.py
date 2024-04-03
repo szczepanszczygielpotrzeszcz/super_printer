5511eb4c5c81e5267ac112807d745e1afe8e6ca7
Stefanos <szczepanszczygielpotrzeszcz@interia.pl>
2024-04-03 21:01:11 +0200

    Implement more methods to pritner

--- print.py
+++ print.py
@@ -11,6 +11,14 @@ class Printer:
 	def print_nl(self, data):
 		print(data)
 		print()
+	
+	def print_tuple(self, tpl, nl=False):
+		print(f"The tuple: {tpl}")
+		if nl: print()
+
+	def print_list(self, ls, nl=False):
+		print(f"The list: {ls}")
+		if nl: print()
 
 
 if __name__ == "__main__":
@@ -22,3 +30,8 @@ if __name__ == "__main__":
 	pr.print("abc")
 	pr.print(123)
 	pr.print_nl("abc with nl")
+	print()
+
+	pr.print_tuple((3, 4, 5))
+	pr.print_list([1, 10, 100])
+	print()
