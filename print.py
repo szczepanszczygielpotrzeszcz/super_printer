6659e68f0070ef1b03091a636e6c9cea0efd93d7
Stefanos <skibimilian.maksynski@gmail.com>
2024-04-03 20:55:41 +0200

    Implement simple printer tests

--- print.py
+++ print.py
@@ -11,3 +11,14 @@ class Printer:
 	def print_nl(self, data):
 		print(data)
 		print()
+
+
+if __name__ == "__main__":
+	print("Printer tests.")
+	print()
+
+	pr = Printer()
+
+	pr.print("abc")
+	pr.print(123)
+	pr.print_nl("abc with nl")
